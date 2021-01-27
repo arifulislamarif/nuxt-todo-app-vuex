5de7f4c73076c26efc5ef581d0e8187ab701392a
Ariful Islam Arif <arif14arif15@gmail.com>
2021-01-27 12:00:43 +0600

    vuex action file updated

--- pages/index.vue
+++ pages/index.vue
@@ -4,29 +4,46 @@
     <div class="flex flex-col">
       <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-          <div class="shadow overflow-hidden border-b border-black-200 sm:rounded-lg">
+          <div
+            class="shadow overflow-hidden border-b border-black-200 sm:rounded-lg"
+          >
             <table class="min-w-full divide-y divide-black-200">
               <thead class="bg-black-50">
                 <tr>
-                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-black-500 uppercase tracking-wider">
+                  <th
+                    scope="col"
+                    class="px-6 py-3 text-left text-xs font-medium text-black-500 uppercase tracking-wider"
+                  >
                     SL.
                   </th>
-                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-black-500 uppercase tracking-wider">
+                  <th
+                    scope="col"
+                    class="px-6 py-3 text-left text-xs font-medium text-black-500 uppercase tracking-wider"
+                  >
                     Name
                   </th>
-                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-black-500 uppercase tracking-wider">
+                  <th
+                    scope="col"
+                    class="px-6 py-3 text-left text-xs font-medium text-black-500 uppercase tracking-wider"
+                  >
                     Email
                   </th>
-                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-black-500 uppercase tracking-wider">
+                  <th
+                    scope="col"
+                    class="px-6 py-3 text-left text-xs font-medium text-black-500 uppercase tracking-wider"
+                  >
                     Action
                   </th>
                 </tr>
               </thead>
-              <tbody class="bg-dark divide-y divide-black-200">
-                <tr v-for="(student,index) in students" :key="index">
+              <tbody
+                v-if="students.length > 0"
+                class="bg-dark divide-y divide-black-200"
+              >
+                <tr v-for="(student, index) in students" :key="index">
                   <td class="px-6 py-4 whitespace-nowrap">
                     <div class="text-sm text-black-900">
-                      {{ index+1 }}
+                      {{ index + 1 }}
                     </div>
                   </td>
                   <td class="px-6 py-4 whitespace-nowrap">
@@ -39,14 +56,26 @@
                       {{ student.email }}
                     </div>
                   </td>
-                  <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                    <button href="#" class="text-indigo-600 hover:text-indigo-900">
-                      Edit
-                    </button>
-                    <button class="text-indigo-600 hover:text-indigo-900" @click.prevent="delete_todo(index)">
+                  <td
+                    class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium"
+                  >
+                    <button
+                      class="text-indigo-600 hover:text-indigo-900"
+                      @click.prevent="delete_todo(index)"
+                    >
                       Delete
                     </button>
                   </td>
+                </tr>
+              </tbody>
+              <tbody
+                v-else
+                class="bg-dark divide-y divide-black-200 text-center"
+              >
+                <tr>
+                  <p class="text-center">
+                    Nothing Found!
+                  </p>
                 </tr>
               </tbody>
             </table>
@@ -54,7 +83,9 @@
         </div>
       </div>
     </div>
-    <div class="min-h-screen flex items-center justify-center bg-black-50 py-12 px-4 sm:px-6 lg:px-8">
+    <div
+      class="min-h-screen flex items-center justify-center bg-black-50 py-12 px-4 sm:px-6 lg:px-8"
+    >
       <div class="max-w-md w-full space-y-8">
         <form class="mt-8 space-y-6" @submit.prevent="studentFormSubmit">
           <input type="hidden" name="remember" value="true">
@@ -64,7 +95,7 @@
               <input
                 v-model="name"
                 type="text"
-                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-black-300 placeholder-black-500 text-black-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                class="appearance-none mb-3 rounded-none relative block w-full px-3 py-2 border border-black-300 placeholder-black-500 text-black-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Name"
               >
             </div>
@@ -73,16 +104,16 @@
               <input
                 v-model="email"
                 type="text"
-                class="appearance-none my-5 rounded-none relative block w-full px-3 py-2 border border-black-300 placeholder-black-500 text-black-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                class="appearance-none mb-3 rounded-none relative block w-full px-3 py-2 border border-black-300 placeholder-black-500 text-black-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Email"
               >
             </div>
           </div>
           <div>
-            <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-              <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-                <!-- Heroicon name: lock-closed -->
-              </span>
+            <button
+              type="submit"
+              class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
               Add
             </button>
           </div>
@@ -97,8 +128,7 @@ export default {
   data () {
     return {
       name: '',
-      email: '',
-      isEditing: false
+      email: ''
     }
   },
   computed: {
@@ -112,6 +142,8 @@ export default {
   methods: {
     studentFormSubmit () {
       this.$store.commit('studentAdd', { name: this.name, email: this.email })
+      this.name = null
+      this.email = null
     },
     delete_todo (id) {
       this.$store.state.students.splice(id, 1)
@@ -131,16 +163,8 @@ export default {
 }
 
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
--- store/index.js
+++ store/index.js
@@ -1,20 +1,26 @@
-import axios from 'axios'
+// import axios from 'axios'
 
 export const state = () => ({
   students: []
 })
 
 export const actions = {
-  studentData ({ commit }) {
-    axios({ url: 'https://jsonplaceholder.typicode.com/users', method: 'GET' })
-      .then((resp) => {
-        const users = resp.data
-        commit('fetch_users', users)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
+  async studentData ({ commit }) {
+    const users = await this.$axios.$get('https://jsonplaceholder.typicode.com/users')
+    commit('fetch_users', users)
   }
+
+  // studentData ({ commit }) {
+  //   axios({ url: 'https://jsonplaceholder.typicode.com/users', method: 'GET' })
+  //     .then((resp) => {
+  //       const users = resp.data
+  //       commit('fetch_users', users)
+  //     })
+  //     .catch((err) => {
+  //       console.log(err)
+  //     })
+  // }
+
 }
 
 export const mutations = {
